aeaf415dcc1eaeb8d5619811c727135ba0ad0bab
Fernando <fernando@os.city>
2025-04-06 17:40:08 -0600

    feat: finalizacion de ejercicio y redaccion de readme.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Entregable Clase 3 Modulo 4: Métodos de arreglos
+
+## Descripción
+
+Este proyecto consiste en la práctica del manejo de arreglos de objetos en JavaScript mediante la gestión de productos de una tienda en línea. La aplicación filtra productos según su precio, los ordena alfabéticamente y genera una lista con sus nombres. También se hace uso de funciones como `filter()`, `sort()`, `map()` y de forma opcional `reduce()` para reforzar la comprensión del manejo de datos en JavaScript.
+
+## Enlace del repositorio
+
+[GitHub - Proyecto Métodos de arreglos Clase 3](https://github.com/AilynMza/Estructuras.de.Datos-Clase-2-M4)
+
+## Enlace del GitHub Pages
+
+[GitHub Pages - Proyecto Métodos de arreglos Clase 3](https://ailynmza.github.io/Estructuras.de.Datos-Clase-2-M4/)
+
+## Conclusión
+
+Con este proyecto, reforcé mis conocimientos sobre el uso de métodos de arreglos como `filter`, `sort`, `map` y `reduce`. Aprendí a organizar y transformar datos de forma eficiente usando JavaScript, lo cual es útil para el manejo de información en tiendas en línea o cualquier sistema que requiera ordenar y filtrar datos dinámicamente. 🐱‍💻
+
+![Meme](/imgs/metodos-arreglos.jpeg)
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const listaProductos = document.getElementById('productos-lista');
 const productosOrdenados = document.getElementById('productos-ordenados');
+const nombresProductos = document.getElementById(`nombres-productos`);
+const precioProductos = document.getElementById(`precio-productos`) 
 //arreglo de objetos con al menos 5 productos, cada uno con las propiedades nombre, precio y categoría.
 
 const productos = [
@@ -10,7 +12,7 @@ const productos = [
     { nombre: "Vestido de hada dulce color blanco", precio: 600, categoria: "Coquette" },
 ];
 
-// PRODUCTOS DE MENOS DE 100 PESOS
+// PRODUCTOS DE MENOS DE 100 PESOS - FILTER
 const menorCienPesos = productos.filter(producto => producto.precio < 100);
 console.log(`Productos con Precios Menores a 100 PESOS:`)
 
@@ -21,12 +23,14 @@ menorCienPesos.forEach(producto => {
     listaProductos.appendChild(li); 
 })
 
-//PRODUCTOS POR ORDEN ALFABÉTICP
+//PRODUCTOS POR ORDEN ALFABÉTICO - SORT
 const ordenAlfabetico = productos.sort((a, b) => {
     if (a.nombre < b.nombre) return -1;
     if (a.nombre > b.nombre) return 1;
 });
-console.log(`Productos Ordenados Alfabéticamente:`)
+console.log(`
+    
+Productos Ordenados Alfabéticamente:`)
 
 ordenAlfabetico.forEach(producto => {
     console.log(`-> Nombre: ${producto.nombre}.\nPrecio: $${producto.precio}.\nCategoría: ${producto.categoria}`)
@@ -34,3 +38,28 @@ ordenAlfabetico.forEach(producto => {
     li.textContent = `Nombre: ${producto.nombre}.Precio: $${producto.precio}.Categoría: ${producto.categoria}`;
     productosOrdenados.appendChild(li); 
 })
+
+// PRODUCTOS DONDE SE MUESTRAN SOLO LOS NOMBRES - MAP 
+const productosNombres = productos.map(producto => {
+    return producto.nombre;
+})
+
+console.log(`
+    
+Nombres de Todos los Productos Disponibles:`)
+
+productosNombres.forEach(productoNombre => {
+    console.log(`-> ${productoNombre}`);
+    const li = document.createElement(`li`);
+    li.textContent = `${productoNombre}`;
+    nombresProductos.appendChild(li);
+})
+
+// PRECIO TOTAL DE LOS PRODUCTOS - REDUCE
+const totalSum = productos.reduce((acumulador, producto) => (acumulador + producto.precio), 0)
+console.log(`
+   
+Precio total de los Productos en Tienda: ${totalSum}`)
+precioProductos.textContent = `${totalSum} pesos mexicanos`;
+
+//
